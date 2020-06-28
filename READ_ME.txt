224ccfd92666acf086f72e081dd65555e5161623
timwac <timwaclaws@gmail.com>
2020-06-28 18:46:04 +0100

    Initial commit

---
+++ READ_ME.txt
@@ -0,0 +1,7 @@
+Hive Swagger Docs: https://api.prod.bgchprod.info/omnia/swagger-ui.html#!/Sessions/createSessionUsingPOST
+
+Getting started guide: https://reflectivearchitect.wordpress.com/2017/02/01/getting-into-iot-through-hive/
+
+requests: https://requests.readthedocs.io/
+
+github repo JS implementation: https://github.com/mandalae/hive-data
---
+++ main.py
@@ -0,0 +1,61 @@
+"""
+Main
+
+"""
+import os
+import time
+from datetime import datetime
+
+import requests
+
+TEMPERATURE_DATA_FILE = "temperature_data.txt"
+GLOBAL_HEADERS = {"Content-Type": "application/vnd.alertme.zoo-6.5+json",
+                  "Accept": "application/vnd.alertme.zoo-6.5+json",
+                  "X-Omnia-Client": "Hive Web Dashboard", }
+
+
+def authenticate(username: str, password: str) -> str:
+    """Authenticate with the Hive and return the session id used in further communications"""
+    data = {"sessions": [{
+        "username": username,
+        "password": password,
+        "caller": "WEB",
+    }]}
+    r = requests.post("https://api-prod.bgchprod.info:443/omnia/auth/sessions", json=data, headers=GLOBAL_HEADERS)
+    if r.status_code != 200:
+        return ''
+    dictionary = r.json()
+    session_id_ = dictionary["sessions"][0]["sessionId"]
+    return session_id_
+
+
+def get_temperature(session_id_: str) -> float:
+    """Request the temperature """
+    headers = GLOBAL_HEADERS.copy()
+    headers["X-Omnia-Access-Token"] = session_id_
+    response = requests.get("https://api-prod.bgchprod.info:443/omnia/nodes", headers=headers)
+    return get_temperature_from_response(response)
+
+
+def get_temperature_from_response(response):
+    """Parse the response json for the temperature reading of the hive"""
+    if response.status_code == 200:
+        dictionary = response.json()
+        current_temperature = dictionary["nodes"][3]["features"]["temperature_sensor_v1"]["temperature"][
+            "reportedValue"]
+        return current_temperature
+    return None
+
+
+def run(period=600):
+    """"Get the temperature from the hive"""
+    while 1:
+        session_id = authenticate(os.getenv("HiveU"), os.getenv("HiveP"))
+        if session_id:
+            temperature = get_temperature(session_id)
+            if temperature:
+                export_string = "{0}\t{1}\n".format(datetime.now().isoformat(), str(temperature))
+                print(export_string)
+                with open(TEMPERATURE_DATA_FILE, "a") as temp_file:
+                    temp_file.write(export_string)
+        time.sleep(period)
